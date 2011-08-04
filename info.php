8eda1b14fe6c9037fbd5b21327e410cb86c00246
hertsch <ralf.hertsch@phpmanufaktur.de>
2011-08-04 01:56:39 +0000

    Release 0.48
    git-svn-id: https://server3.subversion-server.com/phpmanufaktur/svn/image_tweak/trunk@22 f5555f96-2496-5c48-90d2-8bbd2222ffa6

--- info.php
+++ info.php
@@ -33,7 +33,7 @@ if (defined('WB_PATH')) {
 $module_directory     = 'image_tweak';
 $module_name          = 'imageTweak';
 $module_function      = 'tool';
-$module_version       = '0.47';
+$module_version       = '0.48';
 $module_status        = 'Stable';
 $module_platform      = '2.8'; 
 $module_author        = 'Ralf Hertsch, Berlin (Germany)';
